16e68cc2f4a2a0ee5e722e7aa94811ff225af335
Kevin Chen <kevinmichaelchen@gmail.com>
2018-07-03 10:09:11 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kevinmichaelchen/vgo-embedded-struct
+
+require github.com/kevinmichaelchen/foo v0.0.0-20180703140757-fe5a29fb8286
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import "github.com/kevinmichaelchen/foo"
+
+type A struct {
+	foo.Cow
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
